7b7cc1f0366ff0597f3824b84c5756006510a620
Stefano Pastore <ste.pastore@studenti.unina.it>
2014-04-12 11:30:18 +0200

    [Bufgix] Fixes bug preventing from getting correct PID as the input length is not equal to 5

--- 01/processi.sh
+++ 01/processi.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
 if [ "$#" -lt "1" ]; then
-	echo 'Error: cannot perform any task with no input.'
-	echo './processes.sh pid1, [pid2, ..., pidN]'
+	echo 'Error: cannot perform any task with no input.' 2>&1
+	echo 'Usage: ./processes.sh pid1, [pid2, ..., pidN]'
 else
 	while [ "$#" -gt "0" ]; do
-	lines=`ps | cut -d' ' -f 1 | grep "^$1$" | wc -l`
+		length=`echo $1 | wc -c`
+		length=$(( $length-1))
+		field_no=$((6-$length))
+		
+		lines=`ps | cut -d' ' -f $field_no | grep "^$1$" | wc -l`
+		echo "Linee trovate: $lines"
 	
-	if [ $lines -gt "0" ]; then
-		`ps --ppid $1 | cut -d' ' -f 1 | grep "^$1$" > Discendenza-$1`
-	fi
-	shift
-done
+		if [ $lines -gt "0" ]; then
+			echo "Creo il file Discendenza-$1"
+			`ps --ppid $1 | cut -d' ' -f $field_no | grep "^$1$" > Discendenza-$1`
+		fi
+		shift
+	done
 fi
